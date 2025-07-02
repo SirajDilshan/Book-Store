ddc6641b2f0a09d078474196c66b4afd56dd96f6
SirajDilshan <sirajdilshan789@gmail.com>
2025-07-02 14:41:13 +0530

    show book list

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,19 @@
 import express from "express";
-import bookModel from "./models/bookModel.js";
+import cors from "cors";
 import connectDB from './db.js';
 import { PORT } from './config.js';
 import bookRoute from "./routes/bookRoute.js";
 
 const app = express();
+
+app.use(cors());
+
+// app.use(cors({
+//   origin: "http://localhost:3000", // only allow this frontend
+//   methods: ["GET", "POST", "PUT", "DELETE"], // allowed methods
+//   credentials: true, // if you're sending cookies or auth headers
+//   allowedHeaders:['Content-Type'],
+// }));
 
 app.use(express.json());
 
